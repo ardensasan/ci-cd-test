79540a09d3b8c8c68856ec3a7073771a3c8f4962
Arden <ardensasan@gmail.com>
2023-06-03 14:21:20 +0800

    add wrong test

--- math.spec.ts
+++ math.spec.ts
@@ -2,6 +2,6 @@ import { add } from "./math";
 
 describe('test', () => {
     it('should add two number correctly', () => {
-        expect(add(1, 1)).toBe(2)
+        expect(add(1, 2)).toBe(2)
     })
 })
